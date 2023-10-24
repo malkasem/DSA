40ea5c68cc7bfc56fda82b638692156acd6ee846
alkasemm <alkasemm@encowayhb.lokal>
2023-10-24 10:26:12 +0200

    GCD Euclid´s Algorithm

--- src/Gcd.java
+++ src/Gcd.java
@@ -5,6 +5,17 @@ This Project To Find GCD " The Greadest Common Divisor" Of Tow Given Number
 
 import java.util.Scanner;
 
+
+/*
+     ###### Algorithm of the Implementatio ######
+
+     1. Find the Minimum Number of both a and b
+     2. Use a for the Loop and start form the i = min and go till One
+     3. Inside the loop, check if i is a Common Divisor
+*/
+
+/* ####
+
 public class Gcd {
 
     static int gcd(int a, int b){
@@ -46,11 +57,53 @@ public class Gcd {
             }
         }*/
 
-        System.out.println("The GCD Is: " + gcd(a,b));
+ //     System.out.println("The GCD Is: " + gcd(a,b));
+  //  }
+//}
 
-    }
-}
+
 
 /*
 ###### The Time complexity For The Previous Code Is Linear O(min(a,b))#####
+                ###### Efficient Brute Force Approach ########
 */
+/*
+                ##### Here Depends on Euclid´s Algorithm ####
+                1. Find the Larger Number
+                2. Find the Difference between the Larger and the Smaller Number(DifNumber)
+                3. Replace the Larger by the (DifNumber)
+                4. Repeat all previous Steps until both Numbers become equal
+                5. Return any one Number, which is the GCD
+*/
+
+// The Time complexity For This Code Is Linear O(max(a,b)
+
+public class Gcd {
+
+    static int euclidGcd(int a, int b) {
+
+
+      while (a != b) {
+          if( a > b) {
+              a = a - b;
+          } else {
+              b = b - a;
+          }
+      }
+        return a;
+    }
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter The First Number: ");
+        int a = scan.nextInt();
+
+        System.out.println("Enter The Second Number: ");
+        int b = scan.nextInt();
+
+        System.out.println("The GCD Is: " + euclidGcd(a,b));
+    }
+}
+
+
+
