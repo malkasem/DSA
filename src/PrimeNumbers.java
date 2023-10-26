af1585dac25a4746b0dc684d2713cf1455abc611
alkasemm <alkasemm@encowayhb.lokal>
2023-10-26 10:14:45 +0200

    enhenced

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -71,23 +71,6 @@ public class PrimeNumbers {
 
 public class PrimeNumbers {
 
-    static String isPrime(int n) {
-
-        while ( n != 1 && n > 0) {
-
-            for (int i = 2; i <= Math.sqrt(n); i++) { // sqrt(n)
-
-                if (n % i == 0) {
-                    return n + " is NOT Prime Number";
-                }
-
-
-            }
-            return n + " is Prime Number";
-        }
-        return "Your Input Is Not Valid! You Can´t Enter 1 or Negative Number";
-    }
-
 
     static String checkPrime(int n) {
 
@@ -100,7 +83,7 @@ public class PrimeNumbers {
                 return n + " is NOT Prime Number";
             }
 
-            for (int i = 5; i*i <= n; i+=6) { //[i*i <= n] = [i =  sqrt(n)]
+            for (int i = 5; i*i <= n; i=i+6) { //[i*i <= n] = [i =  sqrt(n)]
                     if(n%i==0 || n%(i+2)==0){
                         return n + " is NOT Prime Number";
                     }
@@ -117,6 +100,6 @@ public class PrimeNumbers {
         System.out.println("Enter A Number: ");
         int n = scan.nextInt();
 
-        System.out.println(isPrime(n));
+        System.out.println(checkPrime(n));
     }
 }
